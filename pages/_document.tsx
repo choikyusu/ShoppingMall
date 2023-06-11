2d5ecf6294b8b7400d76cd86920520cdd285b96e
최규수 <kyusu88@naver.com>
2023-06-12 08:16:12 +0900

    feat(init): init shopping-mall
    - styled component 추가
    - FE 영역 나눔(header, main, footer)
    - search UI 구현
    - component 폴더 구조

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
 export default function MyDocument() {
   return (
     <Html lang="ko">
-      <Head />
-
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"
+        />
+        <link
+          href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -14,3 +30,30 @@ export default function MyDocument() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async function getInitialProps(
+  context: DocumentContext
+): Promise<DocumentInitialProps> {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = context.renderPage;
+
+  try {
+    context.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(context);
+    return {
+      ...initialProps,
+      styles: [
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>,
+      ],
+    };
+  } finally {
+    sheet.seal();
+  }
+};
