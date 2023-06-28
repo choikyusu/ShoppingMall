80bedf0bfd8d22cd79001911aa21fce13dc75fbf
최규수 <kyusu88@naver.com>
2023-06-28 22:54:31 +0900

    fix: fast refresh 안되는 오류 수정
    - eslint 추가
    - prettier 추가
    - fast refresh 안되는 오류 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: "build",
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the custom server directory to the module search path
+    config.resolve.modules.push(path.resolve("./server"));
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
-import next from "next";
-import userRouter from "./routes/user";
-import authRouter from "./routes/auth";
+import express, { Request, Response } from 'express';
+import next from 'next';
+import userRouter from './routes/user';
+import authRouter from './routes/auth';
 
-const dev = process.env.NODE_ENV === "development";
+const dev = process.env.NODE_ENV !== 'production';
 const port = 3000;
 const app = next({ dev, port });
 
@@ -12,14 +12,14 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.use("/api/user", userRouter);
-  server.use("/api/kakao/auth", authRouter);
+  server.use('/api/user', userRouter);
+  server.use('/api/kakao/auth', authRouter);
 
-  server.all("*", (req: Request, res: Response) => {
+  server.all('*', (req: Request, res: Response) => {
     return handle(req, res);
   });
 
   server.listen(port, (err?: any) => {
-    console.log("ready");
+    console.log('ready');
   });
 });
