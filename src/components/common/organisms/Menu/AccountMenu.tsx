785a3278cebfe67f1eb0fe76c15db3564f33ad54
eunji <ejhwang@seegene.com>
2023-07-03 09:23:57 +0900

    css 정리

--- src/components/common/organisms/Menu/AccountMenu.tsx
+++ src/components/common/organisms/Menu/AccountMenu.tsx
@@ -3,32 +3,38 @@ import { LinkImgLabel } from '../../molecues/LinkImgLabel';
 
 const AccountMenu = () => {
   return (
-    <Styled.Header>
-      <div>
-        <LinkImgLabel
-          linkURL="https://www.kakaocorp.com/page/"
-          buttonName="앱 설치하기"
-        />
-      </div>
-      <Styled.Setting>
-        <LinkImgLabel
-          linkURL="https://www.kakaocorp.com/page/"
-          buttonName="회원가입"
-        />
-        <LinkImgLabel
-          linkURL="https://www.kakaocorp.com/page/"
-          buttonName="로그인"
-        />
-        <LinkImgLabel
-          linkURL="https://www.kakaocorp.com/page/"
-          buttonName="고객센터"
-        />
-      </Styled.Setting>
-    </Styled.Header>
+    <Styled.Wrapper>
+      <Styled.Header>
+        <div>
+          <LinkImgLabel
+            linkURL="https://www.kakaocorp.com/page/"
+            buttonName="앱 설치하기"
+          />
+        </div>
+        <Styled.Setting>
+          <LinkImgLabel
+            linkURL="https://www.kakaocorp.com/page/"
+            buttonName="회원가입"
+          />
+          <LinkImgLabel
+            linkURL="https://www.kakaocorp.com/page/"
+            buttonName="로그인"
+          />
+          <LinkImgLabel
+            linkURL="https://www.kakaocorp.com/page/"
+            buttonName="고객센터"
+          />
+        </Styled.Setting>
+      </Styled.Header>
+    </Styled.Wrapper>
   );
 };
 
 const Styled = {
+  Wrapper: styled.div`
+    padding-left: 150px;
+    padding-right: 150px;
+  `,
   Header: styled.header`
     display: flex;
     justify-content: space-between;
--- src/components/common/organisms/Menu/MainMenu.tsx
+++ src/components/common/organisms/Menu/MainMenu.tsx
@@ -6,30 +6,36 @@ import { VscAccount } from 'react-icons/vsc';
 
 const MainMenu = () => {
   return (
-    <Styled.Header>
-      <LinkImgLabel linkURL="https://www.google.com/" imgPath="Logo.png" />
-      <div>상품</div>
-      <div>클래스</div>
-      <Search />
-      <div>1 리틀 자수정 목걸이</div>
-      <LinkImgLabel
-        linkURL="https://www.naver.com/"
-        imgSvg={<VscAccount />}
-        buttonName="내 정보"
-      />
-      <LinkImgLabel
-        linkURL="https://www.naver.com/"
-        imgSvg={<FaShoppingCart />}
-        buttonName="장바구니"
-      />
-    </Styled.Header>
+    <Styled.Wrapper>
+      <Styled.Header>
+        <LinkImgLabel linkURL="https://www.google.com/" imgPath="Logo.png" />
+        <div>상품</div>
+        <div>클래스</div>
+        <Search />
+        <div>1 리틀 자수정 목걸이</div>
+        <LinkImgLabel
+          linkURL="https://www.naver.com/"
+          imgSvg={<VscAccount />}
+          buttonName="내 정보"
+        />
+        <LinkImgLabel
+          linkURL="https://www.naver.com/"
+          imgSvg={<FaShoppingCart />}
+          buttonName="장바구니"
+        />
+      </Styled.Header>
+    </Styled.Wrapper>
   );
 };
 
 const Styled = {
+  Wrapper: styled.div`
+    padding-left: 150px;
+    padding-right: 150px;
+  `,
   Header: styled.header`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     height: 50px;
   `,
 };
